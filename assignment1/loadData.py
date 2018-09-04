fee2c75c702baa34c4de505a01d312847c5676a1
unknown <xichengh12@gmail.com>
2018-09-04 17:58:46 -0400

    updates

--- assignment1/loadData.py
+++ assignment1/loadData.py
@@ -11,6 +11,6 @@ def loadDataWithTestSet(training_set_path, testing_set_path):
 def loadDataWithoutTestSet(training_set_path, split_to_test=False):
     training_set = pd.read_csv(training_set_path)
     if split_to_test:
-        training_set, testing_set = train_test_split(training_set, test_size=(2/3))
+        training_set, testing_set = train_test_split(training_set, test_size=(1/3))
         return training_set, testing_set
     return training_set
